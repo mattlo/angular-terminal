52537400d295b64ded7b8be3509e85d29b78eada
Matt Lo <mattlo.developer@gmail.com>
2015-01-01 18:42:23 -0600

    added angular wrapper around jquery.terminal

---
+++ angular-terminal.js
@@ -0,0 +1,25 @@
+angular
+.module('angular-terminal', [])
+.directive('angularTerminal', ['$rootScope', function ($rootScope) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			// define namespace
+			var namespace = 'terminal.' + (attrs.angularTerminal || 'default'),
+				t;
+
+			// initialize terminal object
+			t = element.terminal(function(input, terminal) {
+				// user input commands
+				$rootScope.$emit(namespace, input, terminal);
+			}, {
+   				greetings: attrs.greetings || ''
+			});
+
+			// receiving echo commands\
+			$rootScope.$on(namespace + '.echo', function (e, msg) {
+				t.echo(msg);
+			});
+		}
+	};
+}]);
